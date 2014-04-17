b68f7b570b8b145f5b4b03ee3fca264dc99a78f4
Ilya Dyakonov <iluskadyakonov@gmail.com>
2014-04-17 22:18:29 +0200

    added workspace with example project

---
+++ AuthorizerExample/AuthorizerExample/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  AuthorizerExample
+//
+//  Created by Ilya Dyakonov on 17/04/14.
+//  Copyright (c) 2014 ZappyLab. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ AuthorizerExample/AuthorizerExample/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  AuthorizerExample
+//
+//  Created by Ilya Dyakonov on 17/04/14.
+//  Copyright (c) 2014 ZappyLab. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
