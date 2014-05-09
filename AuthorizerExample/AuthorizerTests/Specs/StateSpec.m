0fe0f136bd75e953a78fd3ebca1432fcdc54f8a7
Ilya Dyakonov <iluskadyakonov@gmail.com>
2014-05-09 16:16:48 +0200

    state tests spec

---
+++ AuthorizerExample/AuthorizerTests/Specs/StateSpec.m
@@ -0,0 +1,30 @@
+//
+// Created by Ilya Dyakonov on 09/05/14.
+// Copyright (c) 2014 ZappyLab. All rights reserved.
+//
+//
+
+#import "ZLAAuthorizer.h"
+
+/////////////////////////////////////////////////////
+
+SpecBegin(State)
+
+__block ZLAAuthorizer *authorizer;
+
+beforeEach(^{
+    authorizer = [[ZLAAuthorizer alloc] initWithBaseURL:[NSURL URLWithString:@"http://dev.passageo.com/api/v1"]
+                                          appIdentifier:@"2"];
+});
+
+it(@"should be non signed in by default", ^{
+    expect(authorizer.signedIn).to.beFalsy();
+});
+
+it(@"should should not perform any requests by default", ^{
+    expect(authorizer.performingRequest).to.beFalsy();
+});
+
+SpecEnd
+
+/////////////////////////////////////////////////////
