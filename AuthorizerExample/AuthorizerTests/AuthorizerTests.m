ebb5dc482242ad943f07e2bb786b8243cc1d0f16
Ilya Dyakonov <iluskadyakonov@gmail.com>
2014-05-09 15:30:27 +0200

    authorizer tests

---
+++ AuthorizerExample/AuthorizerTests/AuthorizerTests.m
@@ -0,0 +1,34 @@
+//
+//  AuthorizerTests.m
+//  AuthorizerTests
+//
+//  Created by Ilya Dyakonov on 09/05/14.
+//  Copyright (c) 2014 ZappyLab. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface AuthorizerTests : XCTestCase
+
+@end
+
+@implementation AuthorizerTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
--- Podfile
+++ Podfile
@@ -6,3 +6,8 @@ pod 'ZLNetworkRequestsPerformer', :git => 'https://github.com/zappylab/ZLNetwork
 pod 'AFNetworking', '~> 2.0'
 pod 'NSString+Validation'
 pod 'BlocksKit', '~> 2.2.0'
+
+target :AuthorizerTests do
+	pod 'Specta'
+	pod 'Expecta'
+end
