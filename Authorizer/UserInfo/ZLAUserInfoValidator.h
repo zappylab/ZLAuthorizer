3adc67ae92489c0cb2119aad9680f9b9f6e6ec8d
Yulia Kurnosova <Yulia-Kurnosova@yandex.ru>
2015-10-29 06:47:07 +0300

    first and last name validation

--- Authorizer/UserInfo/ZLAUserInfoValidator.h
+++ Authorizer/UserInfo/ZLAUserInfoValidator.h
@@ -2,8 +2,6 @@
 // Created by Ilya Dyakonov on 01/05/14.
 // Copyright (c) 2014 ZappyLab. All rights reserved.
 //
-//
-
 
 #import <Foundation/Foundation.h>
 
@@ -11,7 +9,10 @@
 
 @interface ZLAUserInfoValidator : NSObject
 
++(BOOL) isFirstNameAcceptable:(NSString *) firstName;
++(BOOL) isLastNameAcceptable:(NSString *) lastName;
 +(BOOL) isFullNameAcceptable:(NSString *) fullName;
+
 +(BOOL) isPasswordAcceptable:(NSString *) password;
 
 @end
--- Authorizer/UserInfo/ZLAUserInfoValidator.m
+++ Authorizer/UserInfo/ZLAUserInfoValidator.m
@@ -2,8 +2,6 @@
 // Created by Ilya Dyakonov on 01/05/14.
 // Copyright (c) 2014 ZappyLab. All rights reserved.
 //
-//
-
 
 #import "ZLAUserInfoValidator.h"
 
@@ -13,13 +11,17 @@ static NSUInteger const ZLAMinPasswordLength = 6;
 
 /////////////////////////////////////////////////////
 
-@interface ZLAUserInfoValidator ()
-
-@end
-
-/////////////////////////////////////////////////////
-
 @implementation ZLAUserInfoValidator
+
++(BOOL) isFirstNameAcceptable:(NSString *) firstName
+{
+    return firstName.length > 0;
+}
+
++(BOOL) isLastNameAcceptable:(NSString *) lastName
+{
+    return lastName.length > 0;
+}
 
 +(BOOL) isFullNameAcceptable:(NSString *) fullName
 {
